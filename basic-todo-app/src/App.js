a5ca47edd76ddf1568074c678002f18b37a0a2ad
Precious Smith <98830144+Capn-Precious@users.noreply.github.com>
2023-01-03 14:11:58 -0600

    adding necessary files

---
+++ basic-todo-app/src/App.js
@@ -0,0 +1,12 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (<div>
+        <h1>To Do:</h1>
+        <Todo title={'Learn React'}/>
+        <Todo title={'Practice PHP'}/>
+      </div>
+  );
+}
+
+export default App;
