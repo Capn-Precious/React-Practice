922c108f59bb5a6a475c8ce528dd3cd8435a1a4e
Precious Smith <98830144+Capn-Precious@users.noreply.github.com>
2023-01-10 12:21:31 -0600

    props children

--- meet-up-app/src/App.js
+++ meet-up-app/src/App.js
@@ -4,18 +4,17 @@ import {Route, Routes} from 'react-router-dom';
 import AllMeetUpsPage from "./pages/AllMeetUps";
 import NewMeetUpsPage from "./pages/NewMeetUps";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
     return (
-        <div>
-            <MainNavigation />
+        <Layout>
             <Routes>
                 <Route path={'/'} element={<AllMeetUpsPage />} />
                 <Route path={'/new-meetup'} element={<NewMeetUpsPage/>} />
                 <Route path={'/favorites'} element={<FavoritesPage/>}/>
             </Routes>
-        </div>
+        </Layout>
     );
 }
 
